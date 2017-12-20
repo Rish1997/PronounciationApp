ca08d46263bfd73e690198947b7e50ec5390611f
Rishabh Chaturvedi <chaturvedirishabh91@gmail.com>
2017-12-20 23:26:31 +0530

    First Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  inputSpeech : String;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private tts : TextToSpeech,public navCtrl: NavController) {
+
+  }
+
+  play(text){
+
+    this.tts.speak(text).then(()=>{
+      console.log("success");
+    }).catch((reason : any)=>{
+      console.log("Error");
+      console.log(reason);
+    })
 
   }
 
